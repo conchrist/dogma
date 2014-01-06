db7f535c6628fe533111f0710a3f068cacb8d786
Christopher Lillthors <christopher.lillthors@gmail.com>
2014-01-06 18:56:08 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,52 +1,17 @@
 package main
 
 import (
-	"code.google.com/p/go.net/websocket"
 	"flag"
-	"fmt"
-	chat "github.com/christopherL91/GoWebSocket2"
+	"github.com/christopherL91/GoWebSocket2/Chat"
 	"html/template"
 	"net/http"
 	"runtime"
 )
 
-type Server struct {
-	clients      []*Client
-	addClient    chan *Client
-	removeClient chan *Client
-	sendAll      chan *chat.MessageStruct
-	messages     []*chat.MessageStruct
-}
-
-//hold all the data to manage to communicate with a client
-func NewServer() *Server {
-	g := Server{
-		clients:      make([]*Client, 0),
-		addClient:    make(chan *Client),
-		removeClient: make(chan *Client),
-		sendAll:      make(chan *chat.MessageStruct),
-		messages:     make(chan *[]chat.MessageStruct, 0),
-	}
-	return &g
-}
-
 func serveMain(rw http.ResponseWriter, req *http.Request) {
 	var template_file, _ = template.ParseFiles("Public/web.html")
 	req.Header.Add("Content-Type", "application/javascript")
 	template_file.Execute(rw, nil)
-}
-
-func websocketServer(ws *websocket.Conn) {
-	ch := make(chan string)
-	go read(ws)
-	for {
-		select {
-		case c := <-ch:
-			fmt.Println(c)
-		default:
-			//do nothing...
-		}
-	}
 }
 
 func init() {
@@ -56,7 +21,8 @@ func init() {
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(websocketServer))
+	server := Chat.NewServer("/echo")
+	go server.Listen()
 	http.HandleFunc("/", serveMain)
 	http.HandleFunc("/Public/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
