d19dd431958ff74a5cd9e26aac615e02acc0a986
lillt <lillt@kth.se>
2014-01-09 20:00:32 +0100

    add license

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MaxHex.  If not, see <http://www.gnu.org/licenses/>.
+ * along with GoWebSocket.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 module.exports = function(grunt) {
